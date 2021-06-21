0fcad27102771a750ee5609e58b8172786d49757
KeyllaPaixao <gomes.cp@yahoo.com>
2021-06-21 16:04:59 -0400

    21062021

---
+++ semana2/objetos/index.js
@@ -0,0 +1,21 @@
+//Respostas interpretação
+//1: 
+//Matheus Nachtergaele
+//Virginia Cavendish
+//{ canal: 'Globo', horario: '14h' }
+
+//2:
+//a- console.log(cachorro) { nome: 'Juca', idade: 3, raca: 'SRD' }
+//a- console.log(gato) { nome: 'Juba', idade: 3, raca: 'SRD' }
+//a- console.log(tartaruga) { nome: 'Juco', idade: 3, raca: 'SRD' }
+
+//b- Faz uma copia da informação sem modificar o original.
+
+//3
+//a-false, indefinido
+//b- backender retornou um booleano, e altura voltou indefinido por que não houve atribuição
+
+
+// escrita do codigo
+
+
