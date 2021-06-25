60f5f21cd8020fd5ce618347aad8bc51b2dfa933
KeyllaPaixao <gomes.cp@yahoo.com>
2021-06-25 14:34:50 -0400

    exercicio sexta

--- semana2/blackjack/nossoJogo.js
+++ semana2/blackjack/nossoJogo.js
@@ -1,12 +1,70 @@
-/**
- * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
- * 
- * 
-    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
-    
-    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
-    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
- * 
- * 
- * 
- */
+alert("Sejam Bem Vindos ao jogo BlackJack")
+
+const jogador = confirm("Você quer iniciar uma Partida?")
+
+const respostaJogador = jogador
+
+switch (respostaJogador){
+   case false:
+       alert('Que pena, então encerramos o jogo, se mudar de ideia volte outra hora.')
+       break
+
+   case true:
+       alert('Que legal! então vamos jogar!')
+       break
+
+}
+
+alert ("Vamos Sortear suas cartas")
+
+const cartaJogador1 = comprarCarta(naipe, numero)
+
+
+alert(cartaJogador1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*function comprarCarta() {
+   // Cria array de cartas
+   const cartas = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+ 
+   // Cria array de naipes
+   const naipes = ["♦️", "♥️", "♣️", "♠️"]
+ 
+   // Sorteia uma carta
+   const numero = cartas[Math.floor(Math.random() * 13)]
+ 
+   // Sorteia um naipe
+   const naipe = naipes[Math.floor(Math.random() * 4)]
+ 
+   let valor
+
+   // Verifica se é uma das letras e coloca o valor correspondente na variável valor
+   if (numero === "A") {
+     valor = 11
+   } else if (numero === "J" || numero === "Q" || numero === "K") {
+     valor = 10
+   } else { // Se nao for uma das letras, só converte a string para número
+     valor = Number(numero)
+   }
+ 
+   // Cria um objeto da carta com as propriedades que vamos precisar: texto e valor
+   const carta = {
+     texto: numero + naipe,
+     valor: valor
+   }
+ 
+   return carta
+ }*/
