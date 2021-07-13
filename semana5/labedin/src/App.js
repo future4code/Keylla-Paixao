a48d56f9eba6aaa38c4a1d134836a17d54a9543b
KeyllaPaixao <gomes.cp@yahoo.com>
2021-07-13 18:00:49 -0400

    mesmo tendo os arquivos no lugar o linl não abre nada

--- semana5/labedin/src/App.js
+++ semana5/labedin/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import CardGrande from './components/CardGrande/CardGrande';
 import ImagemButton from './components/ImagemButton/ImagemButton';
+import CardPequeno from './components/CardPequeno/CardPequeno';
+
 
 function App() {
   return (
@@ -9,9 +11,9 @@ function App() {
       <div className="page-section-container">
         <h2>Dados pessoais</h2>
         <CardGrande 
-          imagem="https://uploads-ssl.webflow.com/5d640f4558306be99cf47a0e/5dd57846babb597b77c6bb1d_PerfilFuture4_cor.png" 
-          nome="Astrodev" 
-          descricao="Oi, eu sou o Astrodev. Sou o chefe dos alunos da Labenu. Adoro pedir e-mails na sexta-feira e esperar os alunos responderem só para responder com uma bronca e dar mais trabalho para eles."
+          imagem="https://scontent.fmao1-1.fna.fbcdn.net/v/t1.6435-9/202951272_100490035635278_518365256874037561_n.jpg?_nc_cat=100&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=9uHyZxYbhMoAX-sFwhg&tn=vZ-2Wik3ayC5qIJO&_nc_ht=scontent.fmao1-1.fna&oh=48fd6f2c9134d7c931c654142a9db27b&oe=60F29CE6" 
+          nome="Keylla Paixao" 
+          descricao="adoro tecnologia, meus pets e o zeca pagodinho."
         />
         
         <ImagemButton 
@@ -21,17 +23,23 @@ function App() {
       </div>
 
       <div className="page-section-container">
+        <CardPequeno  
+          descricao="Email: meuemail@.com" 
+        />
+      </div>
+
+      <div className="page-section-container">
+        <CardPequeno  
+          descricao="Endereço: Minha casa" 
+        />
+      </div>
+
+      <div className="page-section-container">
         <h2>Experiências profissionais</h2>
         <CardGrande 
-          imagem="https://s3.amazonaws.com/future4.com.br/static/headf4-c492117ca2373dc85ca81bf715b3dc2a.png" 
-          nome="Labenu" 
-          descricao="Formando desenvolvedores para o mercado de trabalho!" 
-        />
-        
-        <CardGrande 
-          imagem="https://imagens.canaltech.com.br/empresas/4418.400.jpg" 
-          nome="NASA" 
-          descricao="Apontando defeitos." 
+          imagem="https://img.olx.com.br/images/85/852820116256751.jpg" 
+          nome="Plano A" 
+          descricao="Professora de Ciências" 
         />
       </div>
 
