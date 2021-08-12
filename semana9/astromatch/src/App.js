bb10543c60ed138cc1e6a77498968c6a32d62870
KeyllaPaixao <gomes.cp@yahoo.com>
2021-08-12 17:06:07 -0400

    não andei muito por que novamente due um erro mas tava aparecendo algo na tela

--- semana9/astromatch/src/App.js
+++ semana9/astromatch/src/App.js
@@ -1,10 +1,9 @@
 import React from 'react'
+import Header from './components/Header';
 
-const App = () => {
+ const App = () => {
   return (
-    <div>
-      Astromatch
-    </div>
+    <Header/>
   )
 }
 export default App;
---
+++ semana9/astromatch/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = props => {
+    return (
+        <Container>
+        <div className = "header">
+        <h1>Astromatch</h1> 
+        <h4>&#11088;Seu par ideal foi escrito nas estrelas&#11088;</h4>
+       </div>
+        </Container>
+    )
+}
+
+export default Header
