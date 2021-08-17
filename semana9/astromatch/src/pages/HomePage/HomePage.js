fc3a82f8dbe9de85de02d5f32873009848405155
KeyllaPaixao <gomes.cp@yahoo.com>
2021-08-17 07:24:12 -0400

    testando o terminal

--- semana9/astromatch/src/pages/HomePage/HomePage.js
+++ semana9/astromatch/src/pages/HomePage/HomePage.js
@@ -1,22 +1,15 @@
 import React from 'react'
 
-import Button from '@material-ui/core/Button';
-import PeopleAlt from "@material-ui/icons/PeopleAlt";
 
-import { ContainerHeader, Title } from './StyledHomePage';
-
+const myfunction = () => {
+    console.log("CLICKED");
+    }
 
 function HomePage (props) {
     return (
-        <ContainerHeader>
-            {props.CurrentPage === 'profile' ? <div></div> : <Button  onClick={props.OnClickChangePage}><PeopleAlt/></Button>}
-            <Title>
-                <h1>Astro Match</h1>
-            </Title>
-            {props.CurrentPage === 'profile' ? <Button size="large" onClick={props.OnClickChangePage} ><PeopleAlt/></Button> : <div></div>}
-
-            
-        </ContainerHeader>
+        <div>
+            <button> onClick = () => {this.myfunction} /></button>
+        </div>
     )
 }
 
